9fce32e2fc18ef64edfb9b77fe1e7e5094536a73
LinhNguyen2k <55703510+anhlinh27092000@users.noreply.github.com>
2020-05-18 00:10:55 +0700

    Bai Test

---
+++ BTVNBuoi01/BaiTap03/BaiTap03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("\tCac uoc cua " + a + " la:");
+            for(int i=1; i<a;i++)
+            {
+                if (a % i == 0)
+                    Console.Write(i+"\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
