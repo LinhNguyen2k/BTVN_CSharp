d8a7542bedd7af3cdd977948cde6591358956aae
LinhNguyen2k <55703510+anhlinh27092000@users.noreply.github.com>
2020-05-18 00:18:24 +0700

    Bai Test

---
+++ BaiKiemTra/BaiTest/NhanVien.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTest
+{
+    class NhanVien
+    {
+        private int manv;
+        private String hoTen;
+        private float hesoLuong;
+        private int soNgayCong;
+        private float troCap;
+
+        public int Manv { get => manv; set => manv = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public float HesoLuong { get => hesoLuong; set => hesoLuong = value; }
+        public int SoNgayCong { get => soNgayCong; set => soNgayCong = value; }
+        public float TroCap { get => troCap; set => troCap = value; }
+
+        public override string ToString()
+        {
+            return ("\tManv:" + manv + ",\tHoTen:" + HoTen + ",\tHeSoLuong:" + hesoLuong + ",\tSoNgayCong:" + soNgayCong + ",\tTroCap:" + troCap + ",\tTinhLuog:" + TinhLuong());
+        }
+        public float TinhLuong()
+        {
+            float TinhLuong = hesoLuong * 150000 * soNgayCong;
+            if (soNgayCong >= 26)
+            {
+                TinhLuong += troCap;
+            }
+            else
+            {
+                TinhLuong -=((26-SoNgayCong)*hesoLuong*150000+(26-SoNgayCong)*hesoLuong*10000); 
+            }
+            return TinhLuong;
+        }
+        public void nhap()
+        {
+            Console.WriteLine("Nhap thong tin nhan vien\n");
+            Console.WriteLine("Nhap ma nhan vien:");
+            manv = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap ho ten:");
+            hoTen = Console.ReadLine();
+            Console.WriteLine("Nhap he so luong:");
+            hesoLuong = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so ngay cong:");
+            soNgayCong = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap tro cap:");
+            troCap = float.Parse(Console.ReadLine());
+        }
+
+    }
+}
---
+++ BaiKiemTra/BaiTest/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTest
+{
+    class Program
+    {
+        public static void TimKiem()
+        {
+            String name;
+            Console.WriteLine("Nhap ten can tim kiem:");
+            int dem = 0;
+            name = (Console.ReadLine());
+            foreach (NhanVien x in list)
+            {
+                if(x.HoTen.Contains(name))
+                {
+                    dem++;
+                    Console.WriteLine(x.ToString());
+                }
+            }
+            if(dem==0)
+            {
+                Console.WriteLine("\n------Khong co-------");
+            }
+         
+
+        }
+        public static void Sort()
+        {
+          
+            for(int i=0;i<n-1;i++)
+            {
+                for(int j=i+1;j<n;j++)
+                {
+                    if (list[i].TinhLuong() > list[j].TinhLuong()) ;
+                    {
+                        NhanVien temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+            Xuat();
+        }
+        public static void Xuat()
+        {
+            foreach (NhanVien x in list)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        public static int n;
+        public static List<NhanVien> list = new List<NhanVien>();
+        
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Nhap so nhan vien:");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i=0;i<n;i++)
+            {
+                Console.WriteLine("Nhap nhan vien thu :"+(i+1));
+                NhanVien a = new NhanVien();
+                a.nhap();
+                list.Add(a);
+
+            }
+            Console.WriteLine("\n\t\t-------Danh Sach Ban Vua Nhap-------");
+            foreach (NhanVien x in list)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\n\t\t-------Danh sach theo ten ban vua tim-------");
+            TimKiem();
+            Console.WriteLine("\n\t\t-------Mang Sau Khi Sort-------");
+            Sort();
+            Console.ReadKey();
+        }
+        
+    }
+}
